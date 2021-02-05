3ca06a478c28b98dd3b004b0fcf06b2e1ba5649c
netscro <kovalev_evgeniy@list.ru>
2021-02-05 16:24:18 +0200

    create firs scraper to scrap name, age, profession, and save it in people.json file

--- work_ua/work_ua/spiders/workua.py
+++ work_ua/work_ua/spiders/workua.py
@@ -4,7 +4,22 @@ import scrapy
 class WorkuaSpider(scrapy.Spider):
     name = 'workua'
     allowed_domains = ['work.ua']
-    start_urls = ['http://work.ua/']
+    start_urls = ['https://www.work.ua/ru/resumes-kharkiv/']
 
     def parse(self, response):
-        pass
+
+        for person in response.css('div.card.resume-link'):
+            name = person.css('div > b::text').get()
+            age = person.css('div > span:nth-child(3)::text').get()
+            if '\xa0' in str(age):
+                age = str(age).replace('\xa0', ' ')
+            else:
+                age = str(age).replace('· ', 'None')
+            profession = person.css('h2 > a::text').get()
+            # print(name, str(age).replace('·', 'None'), profession)
+
+            yield {
+                'name': name,
+                'age': str(age).replace('\xa0', ' '),
+                'profession': profession
+            }
