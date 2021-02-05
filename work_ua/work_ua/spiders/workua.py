074bbfc0d19b66634640efa94ef6497cb8b5ab4c
netscro <kovalev_evgeniy@list.ru>
2021-02-05 17:56:52 +0200

    add next page generator

--- work_ua/work_ua/spiders/workua.py
+++ work_ua/work_ua/spiders/workua.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy import Request
 
 from work_ua.items import PeopleItem
 
@@ -7,23 +8,34 @@ class WorkuaSpider(scrapy.Spider):
     name = 'workua'
     allowed_domains = ['work.ua']
     start_urls = ['https://www.work.ua/ru/resumes-kharkiv/']
+    site_url = 'https://www.work.ua'
 
     def parse(self, response):
 
         for person in response.css('div.card.resume-link'):
             name = person.css('div > b::text').get()
+
             age = person.css('div > span:nth-child(3)::text').get()
             if '\xa0' in str(age):
                 age = str(age).replace('\xa0', ' ')
             else:
                 age = str(age).replace('· ', 'None')
+
             profession = person.css('h2 > a::text').get()
 
             # структура json документа при помощи items.py
             people_item = PeopleItem()
-            people_item['name'] = name
+            people_item['name'] = name.strip()
             people_item['age'] = age
             people_item['profession'] = profession
 
             # возвращаем по одному человеку через генератор
             yield people_item
+
+        # переход по страницам пагинации
+        next_page = response.css('ul.pagination-small li a::attr(href)').getall()
+        if next_page:
+            next_url = self.site_url + next_page[-1]
+            yield Request(next_url)
+
+
